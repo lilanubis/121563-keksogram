d14fea436de41b2bdce9a9b7a4abc58cb76766f9
lilanubis <olgabaryshnikova@MacBook-Pro.local>
2015-11-14 03:38:43 +0100

    all function properties added

--- js/check.js
+++ js/check.js
@@ -1,13 +1,37 @@
-function getMessage (a, b) {
-  if (typeof a == 'boolean') {
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
     if (a) {
-      return "Переданное GIF-изображение анимировано и содержит [" + b + "] кадров";
+      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
     } else {
       return "Переданное GIF-изображение не анимировано";
     }
-  } else if (typeof a == 'number') {
+  }
+  if (typeof a === 'number') {
     if (a) {
-      return "Переданное SVG-изображение содержит [" + a + "] объектов и [" + b * 4 + "] аттрибутов"
+      return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов"
+    }
+  }
+
+  var sum = 0;
+  for (var i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+
+
+  var square = 0;
+
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if(a,b) {
+      return "Общая площадь артефактов сжатия: " + square +  " пикселей";
+    }
+
+  }
+
+  if (Array.isArray(a)) {
+    if (a) {
+      return "Количество красных точек во всех строчках изображения: " + sum + "";
+
     }
   }
 
