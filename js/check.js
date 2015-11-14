8105bcf8518e40b465ffc5e70d9ab352ffa87880
lilanubis <olgabaryshnikova@MacBook-Pro.local>
2015-11-14 22:34:49 +0100

    counted square property

--- js/check.js
+++ js/check.js
@@ -12,23 +12,30 @@ function getMessage(a, b) {
     }
   }
 
-  var sum = 0;
-  for (var i = 0; i < a.length; i++) {
-    sum += a[i];
-  }
 
-
-  var square = 0;
 
 
   if (Array.isArray(a) && Array.isArray(b)) {
+    var square = 0;
+    for (var i = 0; i < b.length; i++) {
+      var d = a[i]*b[i];
+      square += d;
+    }
     if(a,b) {
       return "Общая площадь артефактов сжатия: " + square +  " пикселей";
     }
 
   }
 
+
+
   if (Array.isArray(a)) {
+
+      var sum = 0;
+      for (var i = 0; i < a.length; i++) {
+        sum += a[i];
+      }
+
     if (a) {
       return "Количество красных точек во всех строчках изображения: " + sum + "";
 
