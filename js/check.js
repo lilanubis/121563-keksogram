ea5277ae8975aa58ab2868c980c9580f3174367b
lilanubis <olgabaryshnikova@MacBook-Pro.local>
2015-11-14 23:10:35 +0100

    amendments made

--- js/check.js
+++ js/check.js
@@ -1,46 +1,38 @@
 function getMessage(a, b) {
-  if (typeof a === 'boolean') {
-    if (a) {
-      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-    } else {
-      return "Переданное GIF-изображение не анимировано";
-    }
-  }
-  if (typeof a === 'number') {
-    if (a) {
-      return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов"
-    }
-  }
-
-
-
-
-  if (Array.isArray(a) && Array.isArray(b)) {
-    var square = 0;
-    for (var i = 0; i < b.length; i++) {
-      var d = a[i]*b[i];
-      square += d;
-    }
-    if(a,b) {
-      return "Общая площадь артефактов сжатия: " + square +  " пикселей";
+    if (typeof a === 'boolean') {
+        if (a) {
+            return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+        } else {
+            return "Переданное GIF-изображение не анимировано";
+        }
     }
 
-  }
+    if (typeof a === 'number') {
+        return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов"
+    }
 
-
-
-  if (Array.isArray(a)) {
-
-      var sum = 0;
-      for (var i = 0; i < a.length; i++) {
-        sum += a[i];
-      }
-
-    if (a) {
-      return "Количество красных точек во всех строчках изображения: " + sum + "";
+    if (Array.isArray(a) && Array.isArray(b)) {
+        var square = 0;
+        for (var i = 0; i < b.length; i++) {
+            var d = a[i] * b[i];
+            square += d;
+        }
+        if (b) {
+            return "Общая площадь артефактов сжатия: " + square + " пикселей";
+        }
 
     }
-  }
+
+    if (Array.isArray(a)) {
+
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+
+        return "Количество красных точек во всех строчках изображения: " + sum + "";
+
+    }
 
 
 }
