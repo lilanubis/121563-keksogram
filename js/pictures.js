02b0feacc2e4c157b7027d16e0733404c9ccb872
lilanubis <olgabaryshnikova@MacBook-Pro.local>
2015-12-02 00:57:29 +0100

    template, unload, onerror added

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var filtersTop = document.querySelector('.filters');
 var container = document.querySelector('.pictures');
 
 
@@ -9,8 +10,29 @@ pictures.forEach(function(pictureForm) {
 });
 
 function getElementFromTemplate(data) {
-  return document.createElement('a');
-  element.classList.add('picture');
+  var template = document.querySelector('#picture-template');
+  //console.log(template);
+  if ('content' in template) {
+    var element = template.content.children[0].cloneNode(true);
+  } else {
+    var element = template.content.children[0].cloneNode(true);
+  }
+
+  element.querySelector('.picture-comments').textContent = data.comments;
+  element.querySelector('.picture-likes').textContent = data.likes;
+  var imgPic = element.querySelector('img');
+
+  var backgroundImage = new Image();
+  backgroundImage.onload = function() {
+    element.style.backgroundImage = 'url(\'' + backgroundImage.src + '\')';
+    element.replaceChild(backgroundImage, imgPic);
+  };
+
+  backgroundImage.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+  filtersTop.classList.remove('hidden');
+  backgroundImage.src = data.url;
 
   return element;
 }
